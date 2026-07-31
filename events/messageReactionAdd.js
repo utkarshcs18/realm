720a704249c4e7eef15028789bcca22ac05033b6
Your Name <utkarshkumar.cs18@gmail.com>
2026-07-31 21:00:32 +0530

    add readme.md

--- events/messageReactionAdd.js
+++
@@ -1,9 +0,0 @@
-const { handleReactionRole } = require("../systems/reactionRoles");
-
-module.exports = {
-  name: "messageReactionAdd",
-  async execute(reaction, user) {
-    if (reaction.partial) await reaction.fetch().catch(() => null);
-    await handleReactionRole(reaction, user, true);
-  }
-};
--- events/messageReactionRemove.js
+++
@@ -1,9 +0,0 @@
-const { handleReactionRole } = require("../systems/reactionRoles");
-
-module.exports = {
-  name: "messageReactionRemove",
-  async execute(reaction, user) {
-    if (reaction.partial) await reaction.fetch().catch(() => null);
-    await handleReactionRole(reaction, user, false);
-  }
-};
